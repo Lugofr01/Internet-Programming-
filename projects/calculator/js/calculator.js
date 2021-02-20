b5263e858ac6f0124e76f8a92fa7395d9c86cb44
Frank <lugolafrank@gmail.com>
2021-02-19 23:46:43 -0600

    my second commit

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -7,15 +7,25 @@ var screen;
 
 
 function enterDigit(digit) {
+    if (screen.innerHTML == "0") {
+        screen.innerHTML = `${digit}`;
+    }
+    else{
+        screen.innerHTML = screen.innerHTML + `${digit}`
+        
+    }
 }
 
 function clear_screen() {
+    screen.innerHTML= `${"0"}`;
 }
 
 function eval_expr() {
+    
 }
 
 function enterOp(operation) {
+    screen.innerHTML = screen.innerHTML + `${operation}`
 }
 
 window.onload = function () {
