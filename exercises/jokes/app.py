a118c31cae1501c83774571ff001e88311a972f0
Anghy Aragon <aragan01@MacBook-Air-de-Anghy.local>
2021-04-02 15:40:50 -0500

    nunca

--- exercises/jokes/app.py
+++ exercises/jokes/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Flask application to use `pyjokes`"""
+"""Flask application to use pyjokes"""
 
 import random
 from typing import List
@@ -12,18 +12,27 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    """Render the template with form"""
-    raise NotImplementedError
+    return render_template("base.html")
 
 
 @app.route("/", methods=["POST"])
 def index_jokes():
-    """Render the template with jokes"""
-    raise NotImplementedError
+    if  "language"in request.form and "category" in request.form:
+        return render_template("base.html", result = send_joke(language=request.form.get("language"), category=request.form.get("category")))
+    else :
+        raise "No data to process"
+
 
 
 def send_joke(
     language: str = "en", category: str = "all", number: int = 1
 ) -> List[str]:
-    """Return a list of jokes"""
-    raise NotImplementedError
+  
+        for index in range(int(number)):
+            power = [pyjokes.get_jokes(language, category)[index]]
+        return power
+    
+
+
+
+#sources 
