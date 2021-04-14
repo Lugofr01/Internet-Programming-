e764540b197b46d02c3af49db96cc5e8de767b5d
Anghy Aragon <aragan01@MacBook-Air-de-Anghy.local>
2021-04-13 22:59:53 -0500

    my commit

--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -2,3 +2,54 @@
 """
 jokes api
 """
+import json
+import pyjokes
+from flask import Flask, jsonify, Response
+from flask_cors import CORS,cross_origin
+
+import random
+
+
+app = Flask(__name__)
+
+CORS(app)
+
+jokelist = []
+
+#not sure on how to structure this 
+@app.route("/api/v1/jokes")
+
+@cross_origin()
+def push_joke():
+    return jsonify(anyjoke = pyjokes.get_jokes(language="en", category="all"))
+
+
+
+
+#not sure on how to structure this 
+@app.route("/api/v1/jokes/<int:joke_id>",methods = ["GET"])
+@cross_origin()
+
+def supply_specific_joke(joke_id:int):
+    global jokelist
+    print(len(jokelist))
+
+    if not jokelist:
+        jokelist = pyjokes.get_jokes(language="en",category="all")
+    if joke_id < len(jokelist):
+        return jsonify(specific_joke= jokelist[joke_id])
+
+    return jsonify(specific_joke = jokelist[joke_id%len(jokelist)])
+
+
+  
+
+
+if __name__ == "__main__":
+    
+    app.run
+
+
+
+
+
