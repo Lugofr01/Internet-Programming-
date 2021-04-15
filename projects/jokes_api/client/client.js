273c7759bac600439f449a32fa3dd5eaa40ec703
Anghy Aragon <aragan01@MacBook-Air-de-Anghy.local>
2021-04-14 22:59:02 -0500

    done

--- projects/jokes_api/client/client.js
+++ projects/jokes_api/client/client.js
@@ -1,3 +1,24 @@
 /* jshint esversion: 8 */
 /* jshint browser: true */
 /* jshint node: true */
+'use strict';
+
+const BASE_URL ="https://localhost:5000/api/v1/jokes";
+
+const lang=["en","es","de"];
+const cat=["neutral","chuck","all"]; 
+
+async function getJokes(){
+    
+    var lng =document.getElementById("selLang").value;
+    var num =document.getElementById("selNum").value;
+    var cag=document.getElementById("selCat").value;
+    var Jokeid=document.getElementById("jokeID").value;
+    if (Jokeid){
+        jokes= await fetch(`${BASE_URL}/${lng}/${cag}/${num}/${Jokeid}`)
+    .then(response => response.json());
+    }
+   
+    
+    }
+}
